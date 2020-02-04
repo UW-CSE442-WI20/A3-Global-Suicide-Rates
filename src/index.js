addfb2edf9f39d0572f906ac83323a40e2336cc2
Eliza Huang <eliza123@cs.washington.edu>
2020-02-04 12:49:46 -0800

    some code to load the json that doesn't work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+window.onload = start()
+
+document.addEventListener('DOMContentLoaded', function() {
+    alert("Ready!");
+}, false)
+
+function start() {
+    console.log('hello world');
+    $.getJSON("/resources/overall-suicide-rates.json", function(json) {
+        console.log(json); // access the response object
+        console.log(json.data); // access the array
+        console.log(json.data[0]); // access the first object of the array
+        console.log(json.data[0].number); // access the first object proprty of the array
+    })
+    /*$.getJSON( "/resources/overall-suicide-rates.json", function( json ) {
+        console.log( "JSON Data received, name is " + json.name)
+        console.log("a")
+    })*/
+}
 
 
-console.log('hello world');
