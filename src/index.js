5be82bb62b7c9158c91145dedc8fca3ae4da7b55
Tadtiger13 <tadtiger13@gmail.com>
2020-02-04 14:44:50 -0800

    turns out parcel handles json for you

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-window.onload = start()
+window.onload = start();
 
-document.addEventListener('DOMContentLoaded', function() {
-    alert("Ready!");
-}, false)
+import overall_data from '../resources/overall-suicide-rates.json';
+import detailed_data from '../resources/detailed-suicide-rates.json';
 
 function start() {
-    console.log('hello world');
-    $.getJSON("/resources/overall-suicide-rates.json", function(json) {
-        console.log(json); // access the response object
-        console.log(json.data); // access the array
-        console.log(json.data[0]); // access the first object of the array
-        console.log(json.data[0].number); // access the first object proprty of the array
-    })
-    /*$.getJSON( "/resources/overall-suicide-rates.json", function( json ) {
-        console.log( "JSON Data received, name is " + json.name)
-        console.log("a")
-    })*/
+    console.log(overall_data);
+    console.log(detailed_data);
 }
 
 
