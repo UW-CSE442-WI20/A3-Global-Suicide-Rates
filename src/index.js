62b7e7df9376371ce74690de3940409e625ea35c
imiao1997 <imiao@I-MIAOs-Air.local>
2020-02-12 19:34:17 -0800

    fix year

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ var pie_svg = d3.select("#popup")
 
 // Time
 // d3.select('p#value-time') for the year
-var dataTime = d3.range(0, 20).map(function (d) { return new Date(1995 + d, 10, 3); });
+var dataTime = d3.range(0, 19).map(function (d) { return new Date(1995 + d, 10, 3); });
 
 var sliderTime = d3.sliderBottom().min(d3.min(dataTime)).max(d3.max(dataTime))
     .step(1000 * 60 * 60 * 24 * 365).width(inner_width - padding.left).tickFormat(d3.timeFormat("%Y"))
