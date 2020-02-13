dc9c9a1171798e0d6ea85a770473da202ed6b911
Jayson Chen <jayc1127@uw.edu>
2020-02-12 17:08:31 -0800

    add config file

---
+++ src/config.js
@@ -0,0 +1,13 @@
+// set some variables for padding, size, and labels
+export var outer_width = 625;
+export var outer_height = 455;
+export var padding = { top: 30, right: 0, bottom: 30, left: 60 };
+export var inner_width = outer_width - padding.left - padding.right;
+export var inner_height = outer_height - padding.top - padding.bottom;
+export var popup_width = 200;
+export var circle_radius = 6;
+export var x_col = "GDP per Capita ($)";
+export var y_col = "Suicide Rate per 100k People";
+
+// import {outer_width, outer_height, padding, inner_width, inner_height,
+//     popup_width, circle_radius, x_col, y_col} from './config.js';
